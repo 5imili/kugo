d4dc1f401a694f2c53017e561e5be23302137599
guanyuding <dingguanjun@didichuxing.com>
2018-08-17 20:29:35 +0800

    update create.sh

--- test/create.sh
+++ test/create.sh
@@ -9,4 +9,4 @@ EOF
 
 curl -v -X POST \
      -d "$request_body" \
-    'http://127.0.0.1:7878/kugo/api/v2//namespaces/chao/tasks' | python -m json.tool
+    'http://127.0.0.1:7878/kugo/api/v2/namespaces/chao/tasks' | python -m json.tool
